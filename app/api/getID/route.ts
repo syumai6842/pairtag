9aa557f51fb4628cb98e3f5a9fbe62efbef3c537
ところ <syuumai6842@gmail.com>
2024-10-30 10:46:47 +0900

    fixed ESLint

--- app/api/getID/route.ts
+++ app/api/getID/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest): NextResponse {
     // GET /api/users リクエストの処理
-    return NextResponse.json({ message: "GET request is not allowed" });
+    console.log(request.json());
+    return NextResponse.json({ message: "GET request is not " });
 }
 
 export async function POST(request: NextRequest): Promise<NextResponse<unknown>> {
