0b13a997dfb229f6df1dd0844b9f4fa98fb04a1e
ところ <syuumai6842@gmail.com>
2024-10-30 10:42:28 +0900

    make api endpoint

---
+++ app/api/getID/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest): NextResponse {
+    // GET /api/users リクエストの処理
+    return NextResponse.json({ message: "GET request is not allowed" });
+}
+
+export async function POST(request: NextRequest): Promise<NextResponse<unknown>> {
+    const { id } = await request.json();
+    // POST /api/users リクエストの処理
+    return NextResponse.json({ message: `Received ID: ${id}` });
+}
