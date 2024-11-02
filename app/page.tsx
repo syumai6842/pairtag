ebf1dab8adb33b5a6ccf5220b4ef26f1556bcd25
ところ <syuumai6842@gmail.com>
2024-11-02 15:06:33 +0900

    change title size

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div className={`min-h-screen flex items-center justify-center`} style={{ backgroundColor: 'var(--primary)' }}>
-      <img src="img/pairtaglogo.png" alt="Logo" className="h-auto w-72" />
+      <img src="img/pairtaglogo.png" alt="Logo" className="h-auto w-64" />
     </div>
   );
 }
