599cc88e78596e538b79d14887fdf66e701b226b
市川和 <>
2024-11-02 15:34:47 +0900

    コミット

--- app/Entrance/page.tsx
+++ app/Entrance/page.tsx
@@ -1,10 +1,10 @@
-
+import "../globals.css";
 
 export default function Entrance() {
 
   return (
     <div className="min-h-screen flex items-center justify-center" style={{ backgroundColor: 'var(--primary)' }}>
-      <h1 className="text-white text-4xl">Welcome to the Entrance Page</h1>
+      <h1 className="text-white text-4xl font-['nitalago-ruika'] font-thin">Welcome to the Entrance Page</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'geist-sans': ['var(--font-geist-sans)'],
+        'geist-mono': ['var(--font-geist-mono)'],
+      },
     },
   },
   plugins: [],
