a28b2da00fbed08d71cc0f55dbe22e75e5eac099
ところ <syuumai6842@gmail.com>
2024-11-02 16:51:11 +0900

    merge

--- app/Entrance/page.tsx
+++ app/Entrance/page.tsx
@@ -7,12 +7,15 @@ export default function Entrance() {
     <AnimatePresence>
       <motion.div
         key="entrance"
-        className="min-h-screen flex items-center justify-center"
+        className="min-h-screen flex flex-col items-center justify-center translate-y-[-2vh]"
         style={{ backgroundColor: 'var(--primary)' }}
         initial={{ y: 1000 }}
         animate={{ y: 0 }}
         transition={{ duration: 1 }}
       >
+        <div className="bg-[#2B2B2B] text-white px-24 py-3 mb-8 rounded-3xl">
+          <p className="text-4xl font-bold tracking-tighter">選ぶ</p>
+        </div>
         <div className="flex items-center w-full whitespace-nowrap">
           <div className="h-[1px] bg-white w-[35vw]"></div>
           <h1 className="text-5xl font-bold tracking-tighter text-transparent [-webkit-text-stroke:0.75px_white] mx-4">役職はどっち？</h1>
