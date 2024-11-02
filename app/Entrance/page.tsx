a039677c0ac7565175b4c2165b97dd2a5f26969c
市川和 <>
2024-11-02 16:00:40 +0900

    コミットするよぉー

--- app/Entrance/page.tsx
+++ app/Entrance/page.tsx
@@ -1,10 +1,13 @@
 import "../globals.css";
-
 export default function Entrance() {
-
   return (
-    <div className="min-h-screen flex items-center justify-center" style={{ backgroundColor: 'var(--primary)' }}>
-      <h1 className="text-white text-4xl font-['nitalago-ruika'] font-thin">Welcome to the Entrance Page</h1>
+    <div className="min-h-screen flex items-center justify-center translate-y-[-2vh]" style={{ backgroundColor: 'var(--primary)' }}>
+      <div className="flex items-center w-full whitespace-nowrap">
+        <div className="h-[1px] bg-white w-[35vw]"></div>
+        <h1 className="text-5xl font-bold tracking-tighter text-transparent [-webkit-text-stroke:0.75px_white] mx-4">役職はどっち？</h1>
+        <div className="h-[1px] bg-white w-[35vw]"></div>
+      </div>
     </div>
   );
 }
+
